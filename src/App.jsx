9d892d36d722856a17cc0cf9ce1eb69b1bb240bd
Marius Rundereim <marius.rundereim@gmail.com>
2024-06-14 23:23:25 +0200

    adding project card

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import Justify from "./components/justify";
 import Hero from "./components/sections/Hero";
-
+import ProjectCard from "./components/core/ProjectCard";
 function App() {
   return (
     <>
       <Hero />
+      <div>
+        <ProjectCard
+          image="https://via.placeholder.com/800x600"
+          title="Project title"
+          assignment="Eksamen"
+          date="Mai 2024"
+        />
+      </div>
     </>
   );
 }
